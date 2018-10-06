b43aadabc12dd6736f2d1d7f4edd0430fdc682ff
yudonlee <2017030119@hanyang.ac.kr>
2018-10-06 16:30:30 +0900

    aa

--- algorithms/Heapsort.cpp
+++ algorithms/Heapsort.cpp
@@ -1,56 +1,68 @@
 /* 2017030119_Leeyudon Heapsort */
 #include <stdio.h>
 #include <stdlib.h>
-#define Parent(a) (a-1)/2;
-#define Left(a)  2*a+1;
-#define Right(a) 2*a+2;
+#define Left(a)  2*a;
+#define Right(a) 2*a+1;
 void Swap(int& a,int& b){
   int t;
   t=a;
   a=b;
   b=t;
 }
-void Heapify(int* arr,int n,int i){
-  int l = Left(i);
-  int r = Right(i);
-  int max =i;
-  if(arr[i]<arr[l] && l<n)
-    max = l;
-  else
-    max = i;
-  if(arr[max]<arr[r] && r<n)
-    max = r;
-  if(max != i){
-    Swap(arr[i],arr[max]);
-    Heapify(arr,n,max);
+void Heapify(int* arr,int size,int parent){
+  int max = parent;
+  int l =Left(parent);
+  int r= Right(parent);
+  if (size < l) return;
+  else if (size == l) {
+    if (arr[parent] < arr[l]) {
+      max = l;
+    }
   }
+  else {
+    if (arr[l] > arr[r] &&arr[parent]<arr[l])
+      max = l;
+    else if(arr[r]>arr[l] && arr[parent]< arr[parent]<arr[r])
+      max =r;
+  }
+  if (max != parent) {
+    Swap(arr[max], arr[parent]);
+    Heapify(arr,size ,max);
+  }
+  
+}
+void Build_MaxHeap(int* arr,int n){
+  for (int j=n/2;j> 0;j--){
+     Heapify(arr,n,j);
+  }
+}
+void Heapsort(int* arr,int m,int n){
+  Build_MaxHeap(arr,n);
+  int j = n-m;
+  for (int i= n;i>j;i--){  
+    Swap(arr[1],arr[i]);
+    n--;
+    Heapify(arr,n,1);
+    }
 }
 
 int main(){
-  int n,k;
-  scanf("%d %d",&n,&k);
-  int* tmp = (int *)malloc(sizeof(int)*n);
-  for(int i=0;i<n;i++)
+  int n,m;
+  scanf("%d %d",&n,&m);
+  int* tmp = (int *)malloc(sizeof(int)*n+1);
+  for(int i=1;i<n+1;i++)
     scanf("%d",&tmp[i]);
-  for (int j=n/2-1;j>= 0;j--){
-    Heapify(tmp,n,j);
+  Heapsort(tmp,m,n);
+  int *p = tmp+n;
+  while(p>(tmp+n-m)){
+    printf("%d ",*p--);
   }
-  int count = 0;
-  int j = n-1;
-  int l = n- k-1;
-  for (j; j > l; --j) {
-    count++;
-    printf("%d ", tmp[0]);
-    Swap(tmp[0], tmp[j]);
-    n--;
-    Heapify(tmp,n, 0);
-  }
+  printf("\n");
+  for(int i=1;i<=n-m;i++)
+    printf("%d ",tmp[i]);
   
   printf("\n");
-  for (int i = 0; i <n; ++i) {
-    printf("%d ", tmp[i]);
-  }
-  
 
+  free(tmp);
   return 0;
 }
