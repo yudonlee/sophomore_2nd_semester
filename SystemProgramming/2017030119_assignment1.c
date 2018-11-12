5fb0d2ef5a539f454170c922080293546131c836
yudonlee <2017030119@hanyang.ac.kr>
2018-11-12 15:57:58 +0900

    assignment1 final

--- SystemProgramming/2017030119_assignment1.c
+++ SystemProgramming/2017030119_assignment1.c
@@ -17,14 +17,19 @@ int main(int argc,char** argv){
 	//execv function converts current process to file process,so I make child process and throw child process to pointed file process.
 	int status;
 	pid_t pid = fork();
+	//use command line parameter to execute execv function parameter
+	char **new_argv = (char **)malloc(sizeof(char*)*(argc+1));
+	for(int idx = 1;idx<argc; idx++)
+		new_argv[idx-1] = argv[idx];
+	//last parameter must null.
+	new_argv[argc] = NULL;
 	if(pid<0){
 		perror("fork error!");
 	}
 	else if(pid==0){
 		 //execv need NULL as last parameter.(reference Linux manpage)the list of argument must be terminated by NULL pointer.these string shall constitute the argument list availabe to the new process.so the list must be terminated by null Pointer. 
-		argv[argc] = (char*)0;
 		//if execv function is failed,then return -1.so this if statement handle them.
-		if(execv("/bin/ls",argv+1)<0){
+		if(execv("/bin/ls",new_argv)<0){
 			fprintf(stderr,"Error execution!\n");
 			exit(1);
 		}
