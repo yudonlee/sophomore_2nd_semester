2bc7221ff22d6559acb9bd5fe2dc9a8e292d5952
yudonlee <2017030119@hanyang.ac.kr>
2018-11-12 14:59:43 +0900

    Systemprogramming assignment1 modifed 2018.11.12 2:59pm

--- SystemProgramming/2017030119_assignment1.c
+++ SystemProgramming/2017030119_assignment1.c
@@ -4,28 +4,34 @@
 #include <wait.h>
 #include <string.h>
 int main(int argc,char** argv){
+	//if argc <2 ,then we doesn't get command ls.so print "enter command".
 	if(argc <2){
 		fprintf(stderr,"Please enter command\n");
 		exit(1);
 	}
+	//if argv[1] is not equal to ls,then we should not execute ls command.
 	if(strcmp(argv[1],"ls")){
 		fprintf(stderr,"Please input right command ""ls""\n");
 		exit(1);
 	}
+	//execv function converts current process to file process,so I make child process and throw child process to pointed file process.
+	int status;
 	pid_t pid = fork();
 	if(pid<0){
 		perror("fork error!");
 	}
 	else if(pid==0){
-		argv[argc] = (char*)0; //execv need NULL as last parameter.(reference Linux manpage)the list of argument must be terminated by NULL pointer.these string shall constitute the argument list availabe to the new process.so the list must be terminated by null Pointer. 
+		 //execv need NULL as last parameter.(reference Linux manpage)the list of argument must be terminated by NULL pointer.these string shall constitute the argument list availabe to the new process.so the list must be terminated by null Pointer. 
+		argv[argc] = (char*)0;
+		//if execv function is failed,then return -1.so this if statement handle them.
 		if(execv("/bin/ls",argv+1)<0){
 			fprintf(stderr,"Error execution!\n");
 			exit(1);
 		}
 	}
 	else{	
-		wait(0); //wait for child process exit
-		exit(1);
+		wait(&status); //wait for child process exit
+		exit(1);  //parent process is terminated.
 	}
 	return 0;
 }
